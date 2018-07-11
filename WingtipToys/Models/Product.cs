0b52c88599f740bf09b623745f9722d88ca1b508
Marcos Costa Santos <mcs1901@outlook.com>
2018-07-11 10:41:36 -0300

    Initializer Class

---
+++ WingtipToys/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WingtipToys.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Description)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+        
+    }
+}
